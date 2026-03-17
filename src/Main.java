f9161551e6585956b5cb77050c02ca460bd4d2d2
Wilson <wil.vs@outlook.com>
2026-03-17 12:32:09 -0600

    crear pila en main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        BitacoraStack.imprimir();
+        System.out.println("----------------------------------");
+        System.out.println("|Implementación de TDA Pila - UDV|");
+        System.out.println("----------------------------------");
+        
+        BitacoraStack pila = new BitacoraStack(5);
+        pila.registrar("Motor de estribor encendido");
+        pila.registrar("Velocidad warp alcanzada");
+        pila.registrar("Señal de comunicación estable");
+        pila.registrar("Anomalía detectada en sector 7");
+        pila.registrar("Escudos al 40%");
+        pila.registrar("ERROR CRÍTICO: fallo en sistema de navegación");
+
+        pila.consultarUltimo();
+
     }
 }
