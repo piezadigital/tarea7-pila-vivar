2abe192f307c5c955f1e45679c6e57fa1c234b44
Wilson <wil.vs@outlook.com>
2026-03-17 12:18:28 -0600

    métodos modificados en BitacoraStack

--- src/BitacoraStack.java
+++ src/BitacoraStack.java
@@ -1,6 +1,42 @@
 public class BitacoraStack {
     
-    public static void imprimir(){
-        System.out.println("Hola mundo!");
+    private String[] pila;
+    private int tope;
+    
+    public BitacoraStack(int tamano){
+        pila = new String[tamano];
+        tope = -1;
+    }
+
+    public boolean estaVacia(){
+        return tope == -1;
+    }
+
+    public int totalEventos(){
+        int total = pila.length;
+        return total;
+    }
+
+    public void registrar(String elemento){
+        if(tope == pila.length -1){
+            System.out.println("La pila se ha llenado");
+            return;
+        }
+        tope++;
+        pila[tope] = elemento;
+    }
+
+    public String consultarUltimo(){
+        if(estaVacia()){
+            throw new IllegalStateException("La Pila está vacía");
+        }
+        return pila[tope];
+    }
+
+    public String eliminarUltimo(){
+        if(estaVacia()){
+            throw new IllegalStateException("La Pila está vacía");
+        }
+        return pila[tope--];
     }
 }
